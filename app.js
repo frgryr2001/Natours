938af6993c9b444b3f556a7a80eaf6b9fbec0074
frgryr2001 <nhanle.lx@gmail.com>
2022-04-09 11:33:50 +0700

    Error and ErrorHandling

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
-
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const userRouter = require('./routes/userRoutes');
 
 if (process.env.NODE_ENV === 'development') {
@@ -25,5 +26,15 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+    // res.status(404).json({
+    //     status: 'fail',
+    //     message: `Can't find ${req.originalUrl} on this server!`
+    // });
+
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+app.use(globalErrorHandler);
 // 4) Start server
 module.exports = app;
